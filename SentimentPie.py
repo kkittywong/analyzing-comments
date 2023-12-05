3edd856fcb99c8662b8fe5e4b94ed2c0e7b6f5f0
kkittywong <151720198+kkittywong@users.noreply.github.com>
2023-12-06 03:23:53 +0800

    Update SentimentPie.py

--- SentimentPie.py
+++ SentimentPie.py
@@ -23,14 +23,14 @@ else:
     table_color = ''   
 
 def pie2():
-    df=pd.read_csv('/Users/mankiwong/Desktop/評論內容.csv')
-    data=df['評論內容']
-    pos_count=0   #積極
-    mid_count=0   #中性
-    neg_count=0   #消極
+    df = pd.read_csv('/Users/mankiwong/Desktop/評論內容.csv')
+    data = df['評論內容']
+    pos_count = 0   #積極
+    mid_count = 0   #中性
+    neg_count = 0   #消極
     for comment in data:
-        comment=str(comment)
-        sentiments_score=SnowNLP(comment).sentiments
+        comment = str(comment)
+        sentiments_score = SnowNLP(comment).sentiments
         if sentiments_score < 0.4:
             neg_count += 1
         elif 0.4 <= sentiments_score <= 0.6:
@@ -39,18 +39,18 @@ def pie2():
             pos_count += 1    
 
     pie = (
-        Pie(init_opts=opts.InitOpts(theme=theme_config, width='450px', height='350px', chart_id=3))
-            .add(series_name='評論區情感分布',
-                data_pair=[['積極', pos_count], 
-                           ['中性', mid_count], 
-                           ['消極', neg_count]],
-                rosetype='radius',
-                radius=['30%', '55%'],
+        Pie(init_opts = opts.InitOpts(theme = theme_config, width = '450px', height = '350px', chart_id = 3))
+            .add(series_name = '評論區情感分布',
+                data_pair = [['積極', pos_count], 
+                            ['中性', mid_count], 
+                            ['消極', neg_count]],
+                rosetype = 'radius',
+                radius = ['30%', '55%'],
                 )
             .set_global_opts(
-            legend_opts=opts.LegendOpts(pos_left='right', orient='vertical')
+            legend_opts = opts.LegendOpts(pos_left = 'right', orient = 'vertical')
         )
-            .set_series_opts(label_opts=opts.LabelOpts(formatter='{b}: {c}')))
+            .set_series_opts(label_opts = opts.LabelOpts(formatter = '{b}: {c}')))
     
     pie.render('3.html')
     print('success')
