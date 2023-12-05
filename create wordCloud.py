9dc0f1d6b78f198ea41d3f6096dca2657c384263
kkittywong <151720198+kkittywong@users.noreply.github.com>
2023-12-06 03:26:47 +0800

    Update create wordCloud.py

--- create wordCloud.py
+++ create wordCloud.py
@@ -8,13 +8,13 @@ def cut(text):
     space_wordlist = " ".join([word for word in wordlist_jieba if len(word) > 1])
     return space_wordlist
 
-with open("/Users/mankiwong/Desktop/簿3.txt" ,encoding="utf-16")as file:
-    text=file.read()
-    text=cut(text)
+with open("/Users/mankiwong/Desktop/簿3.txt", encoding = "utf-16") as file:
+    text = file.read()
+    text = cut(text)
 
-    font='/System/Library/Fonts/Supplemental/Songti.ttc'
-    wordcloud = WordCloud(background_color='white',font_path=font,collocations=False,
-    width=800,height=500,min_font_size=10,max_font_size=300).generate(text)
-    image=wordcloud.to_image()
+    font = '/System/Library/Fonts/Supplemental/Songti.ttc'
+    wordcloud = WordCloud(background_color='white', font_path = font, collocations = False,
+    width = 800, height = 500, min_font_size = 10, max_font_size = 300).generate(text)
+    image = wordcloud.to_image()
     # image.show()
     wordcloud.to_file('词云图.png')
